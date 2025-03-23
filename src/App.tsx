378dab9f6e6e88fe3ce7e878703ba1065a0a44df
itsneufox <156133096+itsneufox@users.noreply.github.com>
2025-03-23 13:31:47 +0000

    Remove unused Link import from App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Block from './components/Block';
 import LogoBlock from './components/LogoBlock';
